f99cf451bb1e49291813d3c8e06b49575adaf1ee
Gurprit Singh <sinbag@users.noreply.github.com>
2016-11-06 07:49:37 -0500

    Update sampler.cpp

--- src/sampler/sampler.cpp
+++ src/sampler/sampler.cpp
@@ -8,6 +8,15 @@
 #include <thread>
 #include <iostream>
 
+#if defined(_OPENMP)
+#ifdef __APPLE__
+#include <libiomp/omp.h>
+#else
+#include <omp.h>
+#endif
+#endif
+
+
 using std::cout ;
 using std::stringstream ;
 using std::endl ;
